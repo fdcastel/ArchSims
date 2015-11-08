f92945ac55dce50dc70edb5b48e4269fbb3759ee
F.D.Castel <fdcastel@gmail.com>
2015-11-08 10:06:28 -0200

    Renames solution to "ArchSims". Removes "ArchSims." prefix from folder names.
    Signed-off-by: F.D.Castel <fdcastel@gmail.com>

--- Build.cmd
+++ Build.cmd
@@ -11,12 +11,21 @@ SETLOCAL
 :: Builds F# Binaries
 ::
 
+IF NOT EXIST .\packages (
+    ECHO *
+    ECHO * Required packages not found. 
+    ECHO *
+    ECHO * Please build once in Visual Studio to download required packages.
+    ECHO *
+    PAUSE && EXIT /B 1
+)
+
 CALL "%VS140COMNTOOLS%"\VsDevCmd.bat
 
-msbuild.exe .\FsArchSims.sln /t:Build /p:Configuration=Release
+msbuild.exe .\ArchSims.sln /t:Build /p:Configuration=Release
 IF ERRORLEVEL 1 PAUSE && EXIT /B 1
 
-mstest.exe /testcontainer:ArchSims.Tests\bin\Release\ArchSims.Tests.dll
+mstest.exe /testcontainer:.\Tests\bin\Release\ArchSims.Tests.dll
 IF ERRORLEVEL 1 PAUSE && EXIT /B 1
 
 
@@ -30,19 +39,19 @@ SET OUT_DIR=.\BuildOutput
 MKDIR %OUT_DIR% 2> NUL
 DEL /S /Q %OUT_DIR%\* > NUL
 
-COPY .\ArchSims.CmdLine\bin\Release\*.exe %OUT_DIR% > NUL
-COPY .\ArchSims.CmdLine\bin\Release\*.dll %OUT_DIR% > NUL
+COPY .\CmdLine\bin\Release\*.exe %OUT_DIR% > NUL
+COPY .\CmdLine\bin\Release\*.dll %OUT_DIR% > NUL
 
 COPY %OUT_DIR%\ArchSims.CmdLine.exe %OUT_DIR%\Ramses.exe > NUL
 COPY %OUT_DIR%\ArchSims.CmdLine.exe %OUT_DIR%\Cesar.exe > NUL
 COPY .\Samples\* %OUT_DIR%\ > NUL
 
 :: ToDo: remove the need for these .config files in runtime ?
-COPY .\ArchSims.CmdLine\bin\Release\ArchSims.CmdLine.exe.config %OUT_DIR% > NUL
-COPY .\ArchSims.CmdLine\bin\Release\ArchSims.CmdLine.exe.config %OUT_DIR%\Ramses.exe.config > NUL
-COPY .\ArchSims.CmdLine\bin\Release\ArchSims.CmdLine.exe.config %OUT_DIR%\Cesar.exe.config > NUL
+COPY .\CmdLine\bin\Release\ArchSims.CmdLine.exe.config %OUT_DIR% > NUL
+COPY .\CmdLine\bin\Release\ArchSims.CmdLine.exe.config %OUT_DIR%\Ramses.exe.config > NUL
+COPY .\CmdLine\bin\Release\ArchSims.CmdLine.exe.config %OUT_DIR%\Cesar.exe.config > NUL
 
-COPY .\ArchSims.Tests\RamsesEmulator.Cesar.txt %OUT_DIR%\ > NUL
+COPY .\Tests\RamsesEmulator.Cesar.txt %OUT_DIR%\ > NUL
 %OUT_DIR%\Cesar.exe %OUT_DIR%\RamsesEmulator.Cesar.txt -Save %OUT_DIR%\RamsesEmulator.Cesar.mem > NUL
 
 ENDLOCAL
